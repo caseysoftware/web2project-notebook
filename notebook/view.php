5ab1278b49566be768192f2c3f3e4a831c29aa3b
Keith Casey <keith@caseysoftware.com>
2017-12-21 00:33:17 -0600

    removed all the redundant logic
    Resolves:
    https://github.com/web2project/modules-notebook/issues/14
    https://github.com/web2project/modules-notebook/issues/12
    https://github.com/web2project/modules-notebook/issues/11

--- notebook/view.php
+++ notebook/view.php
@@ -5,11 +5,10 @@ if (!defined('W2P_BASE_DIR')) {
 
 $note_id = (int) w2PgetParam($_GET, 'note_id', 0);
 
-// check permissions for this record
-$perms = &$AppUI->acl();
-$canEdit = $perms->checkModule($m, 'edit');
-if (!$canEdit) {
-	$AppUI->redirect('m=public&a=access_denied');
+$obj = new CNotebook();
+
+if (!$obj->load($note_id)) {
+    $AppUI->redirect(ACCESS_DENIED);
 }
 
 print '<script type="text/javascript" src="' . w2PgetConfig('base_url') . '/lib/tiny_mce/tiny_mce.js"></script>';
@@ -31,49 +30,6 @@ print '
 </script>
 ';
 
-$note_task = (int) w2PgetParam($_GET, 'task_id', 0);
-$note_parent = (int) w2PgetParam($_GET, 'note_parent', 0);
-$note_project = (int) w2PgetParam($_GET, 'project_id', 0);
-$note_company = (int) w2PgetParam($_GET, 'company_id', 0);
-
-$q = new w2p_Database_Query();
-$q->addQuery('notes.*');
-$q->addQuery('u.user_username');
-$q->addQuery('c.contact_first_name, c.contact_last_name');
-$q->addQuery('cm.contact_first_name AS modified_first_name, cm.contact_last_name AS modified_last_name');
-$q->addQuery('project_id');
-$q->addQuery('task_id, task_name');
-$q->addQuery('company_id, company_name');
-$q->addTable('notes');
-$q->leftJoin('users', 'u', 'note_creator = u.user_id');
-$q->leftJoin('contacts', 'c', 'u.user_contact = c.contact_id');
-$q->leftJoin('users', 'um', 'note_modified_by = um.user_id');
-$q->leftJoin('contacts', 'cm', 'um.user_contact = cm.contact_id');
-$q->leftJoin('companies', 'co', 'company_id = note_company');
-$q->leftJoin('projects', 'p', 'project_id = note_project');
-$q->leftJoin('tasks', 't', 'task_id = note_task');
-$q->addWhere('note_id = ' . (int)$note_id);
-
-// check if this record has dependancies to prevent deletion
-$msg = '';
-$obj = new CNotebook();
-$canDelete = $obj->canDelete($msg, $note_id);
-
-//$obj = null;
-$q->loadObject($obj);
-// load the record data
-if (!$obj && $note_id > 0) {
-	$AppUI->setMsg('Note');
-	$AppUI->setMsg('invalidID', UI_MSG_ERROR, true);
-	$AppUI->redirect();
-}
-
-$df = $AppUI->getPref('SHDATEFORMAT');
-$tf = $AppUI->getPref('TIMEFORMAT');
-
-$note_created = new w2p_Utilities_Date($obj->note_created);
-$note_modified = new w2p_Utilities_Date($obj->note_modified);
-
 // setup the title block
 $ttl = 'View Note';
 $titleBlock = new CTitleBlock($ttl, 'notebook.png', $m, $m . '.' . $a);
@@ -85,48 +41,21 @@ if ($canDelete && $note_id > 0) {
 }
 $titleBlock->show();
 
-if ($obj->note_project) {
-	$note_project = $obj->note_project;
-}
 if ($obj->note_task) {
-	$note_task = $obj->note_task;
-	$task_name = $obj->task_name;
-} elseif ($note_task) {
-	$q->clear();
-	$q->addQuery('task_name');
-	$q->addTable('tasks');
-	$q->addWhere('task_id = ' . (int)$note_task);
-	$task_name = $q->loadResult();
-} else {
-	$task_name = '';
+    $_task = new CTask();
+    $_task->load($obj->note_task);
+    $obj->note_project = $_task->task_project;
 }
 
-if ($obj->note_company) {
-	$note_company = $obj->note_company;
-	$company_name = $obj->company_name;
-} elseif ($note_company) {
-	$q->clear();
-	$q->addQuery('company_name');
-	$q->addTable('companies');
-	$q->addWhere('company_id = ' . (int)$note_company);
-	$company_name = $q->loadResult();
-} else {
-	$company_name = '';
+if ($obj->note_project) {
+    $_project = new CProject();
+    $_project->load($obj->note_project);
+    $obj->note_company = $_project->project_company;
 }
-
-if (intval(w2PgetParam($_GET, 'company_id', 0))) {
-	$extra = array('where' => 'project_active = 1 AND project_company = ' . $note_company);
-} else {
-	$extra = array('where' => 'project_active = 1');
-}
-$project = new CProject();
-$projects = $project->getAllowedRecords($AppUI->user_id, 'projects.project_id,project_name', 'project_name', null, $extra, 'projects');
-$projects = arrayMerge(array('0' => $AppUI->_('All', UI_OUTPUT_JS)), $projects);
 
 $categories = w2PgetSysVal('NoteCategory');
 $status = w2PgetSysVal('NoteStatus');
 ?>
-
 
 <form name="frmDelete" action="?m=notebook" method="post" accept-charset="utf-8">
     <input type="hidden" name="dosql" value="do_note_aed" />
